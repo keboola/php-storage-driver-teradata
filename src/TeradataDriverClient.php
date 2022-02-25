480207a4c80cfa7fa61b1d5a3ba10ce810e0c631
zajca <zajca@zajca.cz>
2022-02-25 15:05:18 +0100

    add more supported commands

--- src/TeradataDriverClient.php
+++ src/TeradataDriverClient.php
@@ -7,12 +7,18 @@ namespace Keboola\StorageDriver\Teradata;
 use Google\Protobuf\Internal\Message;
 use Keboola\StorageDriver\Command\Backend\InitBackendCommand;
 use Keboola\StorageDriver\Command\Backend\RemoveBackendCommand;
+use Keboola\StorageDriver\Command\Bucket\CreateBucketCommand;
+use Keboola\StorageDriver\Command\Bucket\DropBucketCommand;
+use Keboola\StorageDriver\Command\Project\CreateProjectCommand;
 use Keboola\StorageDriver\Contract\Driver\ClientInterface;
 use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
 use Keboola\StorageDriver\Contract\Driver\Exception\CommandNotSupportedException;
 use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
 use Keboola\StorageDriver\Teradata\Handler\Backend\Init\InitBackendHandler;
 use Keboola\StorageDriver\Teradata\Handler\Backend\Remove\RemoveBackendHandler;
+use Keboola\StorageDriver\Teradata\Handler\Bucket\Create\CreateBucketHandler;
+use Keboola\StorageDriver\Teradata\Handler\Bucket\Drop\DropBucketHandler;
+use Keboola\StorageDriver\Teradata\Handler\Project\Create\CreateProjectHandler;
 
 class TeradataDriverClient implements ClientInterface
 {
@@ -40,6 +46,12 @@ class TeradataDriverClient implements ClientInterface
                 return new InitBackendHandler();
             case $command instanceof RemoveBackendCommand:
                 return new RemoveBackendHandler();
+            case $command instanceof CreateProjectCommand:
+                return new CreateProjectHandler();
+            case $command instanceof CreateBucketCommand:
+                return new CreateBucketHandler();
+            case $command instanceof DropBucketCommand:
+                return new DropBucketHandler();
         }
 
         throw new CommandNotSupportedException(get_class($command));
