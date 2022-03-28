08f282c30fca872f370c5c178ecc2d072edcc5ae
zajca <zajca@zajca.cz>
2022-03-28 10:37:55 +0200

    add ImportTableFromTable

---
+++ src/Handler/Table/Import/StageTableDefinitionFactory.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\Teradata\Handler\Table\Import;
+
+use Keboola\Datatype\Definition\Teradata;
+use Keboola\Db\ImportExport\Backend\Teradata\Helper\BackendHelper;
+use Keboola\StorageDriver\Command\Table\TableImportFromTableCommand\SourceTableMapping\ColumnMapping;
+use Keboola\TableBackendUtils\Column\ColumnCollection;
+use Keboola\TableBackendUtils\Column\Teradata\TeradataColumn;
+use Keboola\TableBackendUtils\Table\Teradata\TeradataTableDefinition;
+
+final class StageTableDefinitionFactory
+{
+    private const VARCHAR_MAX = 32000;
+    private const VARCHAR_MAX_TPT = 10666;
+
+    /**
+     * @param string[] $sourceColumnsNames
+     * @param ColumnMapping[] $columnsMapping
+     */
+    public static function createStagingTableDefinitionWithMapping(
+        TeradataTableDefinition $destination,
+        array $columnsMapping
+    ): TeradataTableDefinition {
+        $newDefinitions = [];
+        $primaries = $destination->getPrimaryKeysNames();
+        foreach ($columnsMapping as $columnMapping) {
+            /** @var TeradataColumn $definition */
+            foreach ($destination->getColumnsDefinitions() as $definition) {
+                if ($definition->getColumnName() === $columnMapping->getDestinationColumnName()) {
+                    // if column exists in destination set destination type
+                    $newDefinitions[] = new TeradataColumn(
+                        $columnMapping->getDestinationColumnName(),
+                        new Teradata(
+                            $definition->getColumnDefinition()->getType(),
+                            [
+                                'length' => $definition->getColumnDefinition()->getLength(),
+                                'nullable' => !in_array($columnMapping->getDestinationColumnName(), $primaries),
+                                'default' => $definition->getColumnDefinition()->getDefault(),
+                            ]
+                        )
+                    );
+                    continue 2;
+                }
+            }
+            // if column doesn't exists in destination set default type
+            $newDefinitions[] = self::createVarcharColumn($columnMapping->getDestinationColumnName());
+        }
+
+        return new TeradataTableDefinition(
+            $destination->getSchemaName(),
+            BackendHelper::generateTempTableName(),
+            true,
+            new ColumnCollection($newDefinitions),
+            $destination->getPrimaryKeysNames()
+        );
+    }
+
+    private static function createVarcharColumn(string $columnName): TeradataColumn
+    {
+        return new TeradataColumn(
+            $columnName,
+            new Teradata(
+                Teradata::TYPE_VARCHAR,
+                [
+                    'length' => self::VARCHAR_MAX,
+                    'nullable' => true,
+                ]
+            )
+        );
+    }
+
+    /**
+     * @param string[] $sourceColumnsNames
+     */
+    public static function createStagingTableDefinitionForTPT(
+        TeradataTableDefinition $destination,
+        array $sourceColumnsNames
+    ): TeradataTableDefinition {
+        $newDefinitions = [];
+        // create staging table for source columns in order
+        // but with types from destination
+        // also maintain source columns order
+        foreach ($sourceColumnsNames as $columnName) {
+            /** @var TeradataColumn $definition */
+            foreach ($destination->getColumnsDefinitions() as $definition) {
+                if ($definition->getColumnName() === $columnName) {
+                    $length = $definition->getColumnDefinition()->getLength();
+                    $isVarchar = $definition->getColumnDefinition()->getType() === Teradata::TYPE_VARCHAR;
+                    if ($isVarchar && (int) $length > self::VARCHAR_MAX_TPT) {
+                        // there is nonsense limit in Teradata for TPT to load maximal 10666 characters
+                        $length = self::VARCHAR_MAX_TPT;
+                    }
+                    // if column exists in destination set destination type
+                    $newDefinitions[] = new TeradataColumn(
+                        $columnName,
+                        new Teradata(
+                            $definition->getColumnDefinition()->getType(),
+                            [
+                                'length' => $length,
+                                'nullable' => true,
+                                'default' => $definition->getColumnDefinition()->getDefault(),
+                            ]
+                        )
+                    );
+                    continue 2;
+                }
+            }
+            // if column doesn't exists in destination set default type
+            $newDefinitions[] = self::createVarcharColumnForTPT($columnName);
+        }
+
+        return new TeradataTableDefinition(
+            $destination->getSchemaName(),
+            BackendHelper::generateTempTableName(),
+            true,
+            new ColumnCollection($newDefinitions),
+            [] // <-- ignore primary keys
+        );
+    }
+
+    private static function createVarcharColumnForTPT(string $columnName): TeradataColumn
+    {
+        return new TeradataColumn(
+            $columnName,
+            new Teradata(
+                Teradata::TYPE_VARCHAR,
+                [
+                    'length' => self::VARCHAR_MAX_TPT,
+                    'nullable' => true,
+                ]
+            )
+        );
+    }
+}
---
+++ tests/functional/UseCase/Table/ImportTableFromTableTest.php
@@ -0,0 +1,385 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\FunctionalTests\UseCase\Table;
+
+use Google\Protobuf\Internal\GPBType;
+use Google\Protobuf\Internal\RepeatedField;
+use Keboola\Datatype\Definition\Teradata;
+use Keboola\StorageDriver\Command\Bucket\CreateBucketResponse;
+use Keboola\StorageDriver\Command\Table\ImportExportShared\ImportOptions;
+use Keboola\StorageDriver\Command\Table\ImportExportShared\Table;
+use Keboola\StorageDriver\Command\Table\TableImportFromTableCommand;
+use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
+use Keboola\StorageDriver\FunctionalTests\BaseCase;
+use Keboola\StorageDriver\Teradata\Handler\Table\Import\ImportTableFromTableHandler;
+use Keboola\TableBackendUtils\Column\ColumnCollection;
+use Keboola\TableBackendUtils\Column\Teradata\TeradataColumn;
+use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
+use Keboola\TableBackendUtils\Table\Teradata\TeradataTableDefinition;
+use Keboola\TableBackendUtils\Table\Teradata\TeradataTableQueryBuilder;
+use Keboola\TableBackendUtils\Table\Teradata\TeradataTableReflection;
+use LogicException;
+
+class ImportTableFromTableTest extends BaseCase
+{
+    protected GenericBackendCredentials $projectCredentials;
+
+    protected CreateBucketResponse $bucketResponse;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cleanTestProject();
+
+        [$projectCredentials, $projectResponse] = $this->createTestProject();
+        $this->projectCredentials = $projectCredentials;
+
+        [$bucketResponse,] = $this->createTestBucket($projectCredentials, $projectResponse);
+        $this->bucketResponse = $bucketResponse;
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->cleanTestProject();
+    }
+
+    public function testImportTableFromTableFullLoadNoDedup(): void
+    {
+        $sourceTableName = md5($this->getName()) . '_Test_table';
+        $destinationTableName = md5($this->getName()) . '_Test_table_final';
+        $bucketDatabaseName = $this->bucketResponse->getCreateBucketObjectName();
+        $db = $this->getConnection($this->projectCredentials);
+
+        // create tables
+        $tableSourceDef = new TeradataTableDefinition(
+            $bucketDatabaseName,
+            $sourceTableName,
+            false,
+            new ColumnCollection([
+                TeradataColumn::createGenericColumn('col1'),
+                TeradataColumn::createGenericColumn('col2'),
+                TeradataColumn::createGenericColumn('col3'),
+            ]),
+            []
+        );
+        $qb = new TeradataTableQueryBuilder();
+        $sql = $qb->getCreateTableCommand(
+            $tableSourceDef->getSchemaName(),
+            $tableSourceDef->getTableName(),
+            $tableSourceDef->getColumnsDefinitions(),
+            $tableSourceDef->getPrimaryKeysNames(),
+        );
+        $db->executeStatement($sql);
+        foreach ([['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']] as $i) {
+            $db->executeStatement(sprintf(
+                'INSERT INTO %s.%s VALUES (%s)',
+                TeradataQuote::quoteSingleIdentifier($bucketDatabaseName),
+                TeradataQuote::quoteSingleIdentifier($sourceTableName),
+                implode(',', $i)
+            ));
+        }
+
+        $tableDestDef = new TeradataTableDefinition(
+            $bucketDatabaseName,
+            $destinationTableName,
+            false,
+            new ColumnCollection([
+                TeradataColumn::createGenericColumn('col1'),
+                TeradataColumn::createGenericColumn('col4'), // <- different col rename
+            ]),
+            []
+        );
+        $sql = $qb->getCreateTableCommand(
+            $tableDestDef->getSchemaName(),
+            $tableDestDef->getTableName(),
+            $tableDestDef->getColumnsDefinitions(),
+            $tableDestDef->getPrimaryKeysNames(),
+        );
+        $db->executeStatement($sql);
+
+        $cmd = new TableImportFromTableCommand();
+        $path = new RepeatedField(GPBType::STRING);
+        $path[] = $bucketDatabaseName;
+        $columnMappings = new RepeatedField(
+            GPBType::MESSAGE,
+            TableImportFromTableCommand\SourceTableMapping\ColumnMapping::class
+        );
+        $columnMappings[] = (new TableImportFromTableCommand\SourceTableMapping\ColumnMapping())
+            ->setSourceColumnName('col1')
+            ->setDestinationColumnName('col1');
+        $columnMappings[] = (new TableImportFromTableCommand\SourceTableMapping\ColumnMapping())
+            ->setSourceColumnName('col2')
+            ->setDestinationColumnName('col4');
+        $cmd->setSource(
+            (new TableImportFromTableCommand\SourceTableMapping())
+                ->setPath($path)
+                ->setTableName($sourceTableName)
+                ->setColumnMappings($columnMappings)
+        );
+        $cmd->setDestination(
+            (new Table())
+                ->setPath($path)
+                ->setTableName($destinationTableName)
+        );
+        $cmd->setImportOptions(
+            (new ImportOptions())
+                ->setImportType(ImportOptions\ImportType::FULL)
+                ->setDedupType(ImportOptions\DedupType::INSERT_DUPLICATES)
+                ->setConvertEmptyValuesToNullOnColumns([])
+                ->setNumberOfIgnoredLines(0)
+        );
+
+        $handler = new ImportTableFromTableHandler($this->sessionManager);
+        $handler(
+            $this->projectCredentials,
+            $cmd,
+            []
+        );
+
+        $ref = new TeradataTableReflection($db, $bucketDatabaseName, $destinationTableName);
+        $this->assertSame(3, $ref->getRowsCount());
+
+        // cleanup
+        $qb->getDropTableCommand($tableSourceDef->getSchemaName(), $tableSourceDef->getTableName());
+        $qb->getDropTableCommand($tableDestDef->getSchemaName(), $tableDestDef->getTableName());
+        $db->close();
+    }
+
+    public function testImportTableFromTableFullLoad(): void
+    {
+        $sourceTableName = md5($this->getName()) . '_Test_table';
+        $destinationTableName = md5($this->getName()) . '_Test_table_final';
+        $bucketDatabaseName = $this->bucketResponse->getCreateBucketObjectName();
+        $db = $this->getConnection($this->projectCredentials);
+
+        // create tables
+        $tableSourceDef = new TeradataTableDefinition(
+            $bucketDatabaseName,
+            $sourceTableName,
+            false,
+            new ColumnCollection([
+                TeradataColumn::createGenericColumn('col1'),
+                TeradataColumn::createGenericColumn('col2'),
+                TeradataColumn::createGenericColumn('col3'),
+            ]),
+            []
+        );
+        $qb = new TeradataTableQueryBuilder();
+        $sql = $qb->getCreateTableCommand(
+            $tableSourceDef->getSchemaName(),
+            $tableSourceDef->getTableName(),
+            $tableSourceDef->getColumnsDefinitions(),
+            $tableSourceDef->getPrimaryKeysNames(),
+        );
+        $db->executeStatement($sql);
+        foreach ([['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']] as $i) {
+            $db->executeStatement(sprintf(
+                'INSERT INTO %s.%s VALUES (%s)',
+                TeradataQuote::quoteSingleIdentifier($bucketDatabaseName),
+                TeradataQuote::quoteSingleIdentifier($sourceTableName),
+                implode(',', $i)
+            ));
+        }
+
+        $tableDestDef = new TeradataTableDefinition(
+            $bucketDatabaseName,
+            $destinationTableName,
+            false,
+            new ColumnCollection([
+                TeradataColumn::createGenericColumn('col1'),
+                TeradataColumn::createGenericColumn('col4'), // <- different col rename
+                TeradataColumn::createGenericColumn('_timestamp'),
+            ]),
+            []
+        );
+        $sql = $qb->getCreateTableCommand(
+            $tableDestDef->getSchemaName(),
+            $tableDestDef->getTableName(),
+            $tableDestDef->getColumnsDefinitions(),
+            $tableDestDef->getPrimaryKeysNames(),
+        );
+        $db->executeStatement($sql);
+
+        $cmd = new TableImportFromTableCommand();
+        $path = new RepeatedField(GPBType::STRING);
+        $path[] = $bucketDatabaseName;
+        $columnMappings = new RepeatedField(
+            GPBType::MESSAGE,
+            TableImportFromTableCommand\SourceTableMapping\ColumnMapping::class
+        );
+        $columnMappings[] = (new TableImportFromTableCommand\SourceTableMapping\ColumnMapping())
+            ->setSourceColumnName('col1')
+            ->setDestinationColumnName('col1');
+        $columnMappings[] = (new TableImportFromTableCommand\SourceTableMapping\ColumnMapping())
+            ->setSourceColumnName('col2')
+            ->setDestinationColumnName('col4');
+        $cmd->setSource(
+            (new TableImportFromTableCommand\SourceTableMapping())
+                ->setPath($path)
+                ->setTableName($sourceTableName)
+                ->setColumnMappings($columnMappings)
+        );
+        $cmd->setDestination(
+            (new Table())
+                ->setPath($path)
+                ->setTableName($destinationTableName)
+        );
+        $cmd->setImportOptions(
+            (new ImportOptions())
+                ->setImportType(ImportOptions\ImportType::FULL)
+                ->setDedupType(ImportOptions\DedupType::UPDATE_DUPLICATES)
+                ->setConvertEmptyValuesToNullOnColumns([])
+                ->setNumberOfIgnoredLines(0)
+                ->setTimestampColumn('_timestamp')
+        );
+
+        $handler = new ImportTableFromTableHandler($this->sessionManager);
+        $handler(
+            $this->projectCredentials,
+            $cmd,
+            []
+        );
+
+        $ref = new TeradataTableReflection($db, $bucketDatabaseName, $destinationTableName);
+        $this->assertSame(3, $ref->getRowsCount());
+
+        // cleanup
+        $qb->getDropTableCommand($tableSourceDef->getSchemaName(), $tableSourceDef->getTableName());
+        $qb->getDropTableCommand($tableDestDef->getSchemaName(), $tableDestDef->getTableName());
+        $db->close();
+    }
+
+    public function testImportTableFromTableIncrementalLoad(): void
+    {
+        $sourceTableName = md5($this->getName()) . '_Test_table';
+        $destinationTableName = md5($this->getName()) . '_Test_table_final';
+        $bucketDatabaseName = $this->bucketResponse->getCreateBucketObjectName();
+        $db = $this->getConnection($this->projectCredentials);
+
+        // create tables
+        $tableSourceDef = new TeradataTableDefinition(
+            $bucketDatabaseName,
+            $sourceTableName,
+            false,
+            new ColumnCollection([
+                new TeradataColumn(
+                    'col1',
+                    new Teradata(Teradata::TYPE_VARCHAR, [
+                        'length' => '32000',
+                        'nullable' => false,
+                    ])
+                ),
+                TeradataColumn::createGenericColumn('col2'),
+                TeradataColumn::createGenericColumn('col3'),
+            ]),
+            ['col1']
+        );
+        $qb = new TeradataTableQueryBuilder();
+        $sql = $qb->getCreateTableCommand(
+            $tableSourceDef->getSchemaName(),
+            $tableSourceDef->getTableName(),
+            $tableSourceDef->getColumnsDefinitions(),
+            [], //<-- dont create primary keys allow duplicates
+        );
+        $db->executeStatement($sql);
+        foreach ([['1', '1', '3'], ['2', '2', '2'], ['2', '2', '2'], ['3', '2', '3'], ['4', '4', '4']] as $i) {
+            $db->executeStatement(sprintf(
+                'INSERT INTO %s.%s VALUES (%s)',
+                TeradataQuote::quoteSingleIdentifier($bucketDatabaseName),
+                TeradataQuote::quoteSingleIdentifier($sourceTableName),
+                implode(',', $i)
+            ));
+        }
+
+        $tableDestDef = new TeradataTableDefinition(
+            $bucketDatabaseName,
+            $destinationTableName,
+            false,
+            new ColumnCollection([
+                new TeradataColumn(
+                    'col1',
+                    new Teradata(Teradata::TYPE_VARCHAR, [
+                        'length' => '32000',
+                        'nullable' => false,
+                    ])
+                ),
+                TeradataColumn::createGenericColumn('col4'), // <- different col rename
+                TeradataColumn::createGenericColumn('_timestamp'),
+            ]),
+            ['col1']
+        );
+        $sql = $qb->getCreateTableCommand(
+            $tableDestDef->getSchemaName(),
+            $tableDestDef->getTableName(),
+            $tableDestDef->getColumnsDefinitions(),
+            $tableDestDef->getPrimaryKeysNames(),
+        );
+        $db->executeStatement($sql);
+        foreach ([['1', '1', '1'], ['2', '2', '2'], ['3', '3', '3']] as $i) {
+            $db->executeStatement(sprintf(
+                'INSERT INTO %s.%s VALUES (%s)',
+                TeradataQuote::quoteSingleIdentifier($bucketDatabaseName),
+                TeradataQuote::quoteSingleIdentifier($destinationTableName),
+                implode(',', $i)
+            ));
+        }
+
+        $cmd = new TableImportFromTableCommand();
+        $path = new RepeatedField(GPBType::STRING);
+        $path[] = $bucketDatabaseName;
+        $columnMappings = new RepeatedField(
+            GPBType::MESSAGE,
+            TableImportFromTableCommand\SourceTableMapping\ColumnMapping::class
+        );
+        $columnMappings[] = (new TableImportFromTableCommand\SourceTableMapping\ColumnMapping())
+            ->setSourceColumnName('col1')
+            ->setDestinationColumnName('col1');
+        $columnMappings[] = (new TableImportFromTableCommand\SourceTableMapping\ColumnMapping())
+            ->setSourceColumnName('col2')
+            ->setDestinationColumnName('col4');
+        $cmd->setSource(
+            (new TableImportFromTableCommand\SourceTableMapping())
+                ->setPath($path)
+                ->setTableName($sourceTableName)
+                ->setColumnMappings($columnMappings)
+        );
+        $cmd->setDestination(
+            (new Table())
+                ->setPath($path)
+                ->setTableName($destinationTableName)
+        );
+        $cmd->setImportOptions(
+            (new ImportOptions())
+                ->setImportType(ImportOptions\ImportType::INCREMENTAL)
+                ->setDedupType(ImportOptions\DedupType::UPDATE_DUPLICATES)
+                ->setConvertEmptyValuesToNullOnColumns([])
+                ->setNumberOfIgnoredLines(0)
+                ->setTimestampColumn('_timestamp')
+        );
+
+        $handler = new ImportTableFromTableHandler($this->sessionManager);
+
+        try {
+            $handler(
+                $this->projectCredentials,
+                $cmd,
+                []
+            );
+            $this->fail('Should fail incremental import is not implemented');
+            //$ref = new TeradataTableReflection($db, $bucketDatabaseName, $destinationTableName);
+            // 1 row unique from source, 3 rows deduped from source and destination
+            //$this->assertSame(4, $ref->getRowsCount());
+            // @todo test updated values
+        } catch (LogicException $e) {
+            $this->assertSame('Not implemented', $e->getMessage());
+        }
+
+        // cleanup
+        $qb->getDropTableCommand($tableSourceDef->getSchemaName(), $tableSourceDef->getTableName());
+        $qb->getDropTableCommand($tableDestDef->getSchemaName(), $tableDestDef->getTableName());
+        $db->close();
+    }
+}
