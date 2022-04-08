45b2e81b3967ff04f182ff64e601354a58b4e22f
Martin Junger <martin.junger@keboola.com>
2022-04-09 01:22:46 +0200

    drop workspace object: add command

---
+++ src/Handler/Workspace/DropObject/DropWorkspaceObjectHandler.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\Teradata\Handler\Workspace\DropObject;
+
+use Doctrine\DBAL\Connection;
+use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\Command\Workspace\DropWorkspaceObjectCommand;
+use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
+use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
+use Keboola\StorageDriver\Teradata\TeradataSessionManager;
+use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
+
+final class DropWorkspaceObjectHandler implements DriverCommandHandlerInterface
+{
+    private TeradataSessionManager $manager;
+
+    public function __construct(TeradataSessionManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @inheritDoc
+     * @param GenericBackendCredentials $credentials
+     * @param DropWorkspaceObjectCommand $command
+     */
+    public function __invoke(
+        Message $credentials, // workspace credentials
+        Message $command,
+        array $features
+    ): ?Message {
+        assert($credentials instanceof GenericBackendCredentials);
+        assert($command instanceof DropWorkspaceObjectCommand);
+
+        // validate
+        assert(
+            !empty($command->getWorkspaceObjectName()),
+            'DropWorkspaceObjectCommand.workspaceObjectName is required'
+        );
+        assert(
+            !empty($command->getObjectNameToDrop()),
+            'DropWorkspaceObjectCommand.objectNameToDrop is required'
+        );
+
+        $db = $this->manager->createSession($credentials);
+
+        $isTableExists = $this->isTableExists($db, $command->getWorkspaceObjectName(), $command->getObjectNameToDrop());
+        if ($command->getIgnoreIfNotExists() && !$isTableExists) {
+            $db->close();
+            return null;
+        }
+
+        $db->executeStatement(sprintf(
+            'DROP TABLE %s.%s;',
+            TeradataQuote::quoteSingleIdentifier($command->getWorkspaceObjectName()),
+            TeradataQuote::quoteSingleIdentifier($command->getObjectNameToDrop())
+        ));
+
+        $db->close();
+        return null;
+    }
+
+    private function isTableExists(Connection $connection, string $databaseName, string $tableName): bool
+    {
+        $tables = $connection->fetchAllAssociative(sprintf(
+            'SELECT TableName FROM DBC.TablesVX WHERE DatabaseName = %s AND TableName = %s',
+            TeradataQuote::quote($databaseName),
+            TeradataQuote::quote($tableName)
+        ));
+        return count($tables) === 1;
+    }
+}
---
+++ tests/functional/UseCase/Workspace/DropWorkspaceObjectTest.php
@@ -0,0 +1,144 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\FunctionalTests\UseCase\Workspace;
+
+use Keboola\StorageDriver\Command\Project\CreateProjectResponse;
+use Keboola\StorageDriver\Command\Workspace\CreateWorkspaceResponse;
+use Keboola\StorageDriver\Command\Workspace\DropWorkspaceObjectCommand;
+use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
+use Keboola\StorageDriver\FunctionalTests\BaseCase;
+use Keboola\StorageDriver\Teradata\Handler\Workspace\DropObject\DropWorkspaceObjectHandler;
+use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
+use Throwable;
+
+class DropWorkspaceObjectTest extends BaseCase
+{
+    protected GenericBackendCredentials $projectCredentials;
+
+    protected CreateProjectResponse $projectResponse;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->cleanTestWorkspace();
+        $this->cleanTestProject();
+
+        [$credentials, $response] = $this->createTestProject();
+        $this->projectCredentials = $credentials;
+        $this->projectResponse = $response;
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->cleanTestWorkspace();
+        $this->cleanTestProject();
+    }
+
+    public function testCreateDropWorkspace(): void
+    {
+        // CREATE
+        [$credentials, $response] = $this->createTestWorkspace($this->projectCredentials, $this->projectResponse);
+        $this->assertInstanceOf(GenericBackendCredentials::class, $credentials);
+        $this->assertInstanceOf(CreateWorkspaceResponse::class, $response);
+
+        $db = $this->getConnection($credentials);
+
+        // create tables
+        $db->executeStatement(sprintf(
+            'CREATE TABLE %s."testTable" ("id" INTEGER);',
+            TeradataQuote::quoteSingleIdentifier($response->getWorkspaceObjectName())
+        ));
+        $db->executeStatement(sprintf(
+            'CREATE TABLE %s."testTable2" ("id" INTEGER);',
+            TeradataQuote::quoteSingleIdentifier($response->getWorkspaceObjectName())
+        ));
+
+        // create view
+        $db->executeStatement(sprintf(
+            'CREATE VIEW %s."testView" AS '
+            . 'SELECT "id" FROM %s."testTable";',
+            TeradataQuote::quoteSingleIdentifier($response->getWorkspaceObjectName()),
+            TeradataQuote::quoteSingleIdentifier($response->getWorkspaceObjectName())
+        ));
+
+        $db->close();
+
+        // DROP with BAD TABLE NAME
+        $handler = new DropWorkspaceObjectHandler($this->sessionManager);
+        $command = (new DropWorkspaceObjectCommand())
+            ->setWorkspaceObjectName($response->getWorkspaceObjectName())
+            ->setObjectNameToDrop('objectNotExists');
+
+        try {
+            $handler(
+                $credentials,
+                $command,
+                []
+            );
+            $this->fail('Should fail');
+        } catch (Throwable $e) {
+            $this->assertStringContainsString(
+                sprintf(
+                    "Object '%s.%s' does not exist.",
+                    $response->getWorkspaceObjectName(),
+                    'objectNotExists'
+                ),
+                $e->getMessage()
+            );
+        }
+
+        // DROP with BAD TABLE NAME with IGNORE
+        $handler = new DropWorkspaceObjectHandler($this->sessionManager);
+        $command = (new DropWorkspaceObjectCommand())
+            ->setWorkspaceObjectName($response->getWorkspaceObjectName())
+            ->setObjectNameToDrop('objectNotExists')
+            ->setIgnoreIfNotExists(true);
+
+        $dropResponse = $handler(
+            $credentials,
+            $command,
+            []
+        );
+        $this->assertNull($dropResponse);
+
+        $this->assertTrue($this->isTableExists($db, $response->getWorkspaceObjectName(), 'testTable'));
+        $this->assertTrue($this->isTableExists($db, $response->getWorkspaceObjectName(), 'testTable2'));
+
+        // DROP table
+        $handler = new DropWorkspaceObjectHandler($this->sessionManager);
+        $command = (new DropWorkspaceObjectCommand())
+            ->setWorkspaceObjectName($response->getWorkspaceObjectName())
+            ->setObjectNameToDrop('testTable2');
+
+        $dropResponse = $handler(
+            $credentials,
+            $command,
+            []
+        );
+        $this->assertNull($dropResponse);
+
+        $this->assertTrue($this->isTableExists($db, $response->getWorkspaceObjectName(), 'testTable'));
+        $this->assertFalse($this->isTableExists($db, $response->getWorkspaceObjectName(), 'testTable2'));
+
+        // DROP table used in view
+        $handler = new DropWorkspaceObjectHandler($this->sessionManager);
+        $command = (new DropWorkspaceObjectCommand())
+            ->setWorkspaceObjectName($response->getWorkspaceObjectName())
+            ->setObjectNameToDrop('testTable');
+
+        $dropResponse = $handler(
+            $credentials,
+            $command,
+            []
+        );
+        $this->assertNull($dropResponse);
+
+        $this->assertFalse($this->isTableExists($db, $response->getWorkspaceObjectName(), 'testTable'));
+        $this->assertFalse($this->isTableExists($db, $response->getWorkspaceObjectName(), 'testTable2'));
+
+        $db->close();
+    }
+}
