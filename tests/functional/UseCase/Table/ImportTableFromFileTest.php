cc3280be887977abbdb2a62d9f31a0d70f0262fe
zajca <zajca@zajca.cz>
2022-04-11 21:13:19 +0200

    add new importCompressionProvider for test use phpunit 9.4

--- tests/functional/UseCase/Table/ImportTableFromFileTest.php
+++ tests/functional/UseCase/Table/ImportTableFromFileTest.php
@@ -371,120 +371,6 @@ class ImportTableFromFileTest extends BaseCase
         $db->close();
     }
 
-    /**
-     * @dataProvider importAdapterProvider
-     */
-    public function testImportTableFromTableFullLoadSlicedWithoutDeduplication(int $importAdapter): void
-    {
-        $destinationTableName = md5($this->getName()) . '_Test_table_final';
-        $bucketDatabaseName = $this->bucketResponse->getCreateBucketObjectName();
-        $db = $this->getConnection($this->projectCredentials);
-
-        $this->createAccountsTable($db, $bucketDatabaseName, $destinationTableName);
-        // init some values
-        $db->executeStatement(sprintf(
-        // phpcs:ignore
-            'INSERT INTO %s.%s VALUES (10,448810375,\'init\',0,1,,1,0,\'2012-02-20 09:34:22\',\'ddd\',\'ddd\',1,\'2012-02-20 09:34:22\')',
-            TeradataQuote::quoteSingleIdentifier($bucketDatabaseName),
-            TeradataQuote::quoteSingleIdentifier($destinationTableName),
-        ));
-
-        $cmd = new TableImportFromFileCommand();
-        $path = new RepeatedField(GPBType::STRING);
-        $path[] = $bucketDatabaseName;
-        $cmd->setFileProvider(FileProvider::S3);
-        $cmd->setFileFormat(FileFormat::CSV);
-        $columns = new RepeatedField(GPBType::STRING);
-        $columns[] = 'import';
-        $columns[] = 'isImported';
-        $columns[] = 'id';
-        $columns[] = 'idTwitter';
-        $columns[] = 'name';
-        $columns[] = 'apiLimitExceededDatetime';
-        $columns[] = 'analyzeSentiment';
-        $columns[] = 'importKloutScore';
-        $columns[] = 'timestamp';
-        $columns[] = 'oauthToken';
-        $columns[] = 'oauthSecret';
-        $columns[] = 'idApp';
-        $formatOptions = new Any();
-        $formatOptions->pack(
-            (new TableImportFromFileCommand\CsvTypeOptions())
-                ->setColumnsNames($columns)
-                ->setDelimiter(CsvOptions::DEFAULT_DELIMITER)
-                ->setEnclosure(CsvOptions::DEFAULT_ENCLOSURE)
-                ->setEscapedBy(CsvOptions::DEFAULT_ESCAPED_BY)
-                ->setSourceType(TableImportFromFileCommand\CsvTypeOptions\SourceType::SLICED_FILE)
-                ->setCompression(TableImportFromFileCommand\CsvTypeOptions\Compression::NONE)
-        );
-        $cmd->setFormatTypeOptions($formatOptions);
-        $cmd->setFilePath(
-            (new FilePath())
-                ->setRoot((string) getenv('AWS_S3_BUCKET'))
-                ->setPath('sliced/accounts')
-                ->setFileName('S3.accounts.csvmanifest')
-        );
-        $credentials = new Any();
-        $credentials->pack(
-            (new S3Credentials())
-                ->setKey((string) getenv('AWS_ACCESS_KEY_ID'))
-                ->setSecret((string) getenv('AWS_SECRET_ACCESS_KEY'))
-                ->setRegion((string) getenv('AWS_REGION'))
-        );
-        $cmd->setFileCredentials($credentials);
-        $cmd->setDestination(
-            (new Table())
-                ->setPath($path)
-                ->setTableName($destinationTableName)
-        );
-        $dedupCols = new RepeatedField(GPBType::STRING);
-        $cmd->setImportOptions(
-            (new ImportOptions())
-                ->setImportType(ImportOptions\ImportType::FULL)
-                ->setDedupType(ImportOptions\DedupType::UPDATE_DUPLICATES)
-                ->setDedupColumnsNames($dedupCols)
-                ->setConvertEmptyValuesToNullOnColumns(new RepeatedField(GPBType::STRING))
-                ->setNumberOfIgnoredLines(0)
-                ->setTimestampColumn('_timestamp')
-        );
-        $cmd->setMeta($this->getCmdMeta($importAdapter));
-
-        $handler = new ImportTableFromFileHandler($this->sessionManager);
-        /** @var TableImportResponse $response */
-        $response = $handler(
-            $this->projectCredentials,
-            $cmd,
-            []
-        );
-        $this->assertSame(3, $response->getImportedRowsCount());
-        $this->assertSame(
-            [
-                'import',
-                'isImported',
-                'id',
-                'idTwitter',
-                'name',
-                'apiLimitExceededDatetime',
-                'analyzeSentiment',
-                'importKloutScore',
-                'timestamp',
-                'oauthToken',
-                'oauthSecret',
-                'idApp',
-            ],
-            iterator_to_array($response->getImportedColumns())
-        );
-        $ref = new TeradataTableReflection($db, $bucketDatabaseName, $destinationTableName);
-        // nothing from destination and 3 rows from source
-        $this->assertSame(3, $ref->getRowsCount());
-        $this->assertSame($ref->getRowsCount(), $response->getTableRowsCount());
-
-        // cleanup
-        $qb = new TeradataTableQueryBuilder();
-        $qb->getDropTableCommand($bucketDatabaseName, $destinationTableName);
-        $db->close();
-    }
-
     private function createAccountsTable(Connection $db, string $bucketDatabaseName, string $destinationTableName): void
     {
         $db->executeQuery(sprintf(
@@ -509,10 +395,29 @@ class ImportTableFromFileTest extends BaseCase
     }
 
     /**
-     * @dataProvider importAdapterProvider
+     * @return Generator<string,array{int,int}>
      */
-    public function testImportTableFromTableFullLoadSlicedCompressedWithoutDeduplication(int $importAdapter): void
+    public function importCompressionProvider(): Generator
     {
+        foreach ($this->importAdapterProvider() as $name => $adapter) {
+            yield 'NO Compression ' . $name => [
+                $adapter[0],
+                TableImportFromFileCommand\CsvTypeOptions\Compression::NONE,
+            ];
+            yield 'GZIP ' . $name => [
+                $adapter[0],
+                TableImportFromFileCommand\CsvTypeOptions\Compression::GZIP,
+            ];
+        }
+    }
+
+    /**
+     * @dataProvider importCompressionProvider
+     */
+    public function testImportTableFromTableFullLoadSlicedWithoutDeduplication(
+        int $importAdapter,
+        int $compression
+    ): void {
         $destinationTableName = md5($this->getName()) . '_Test_table_final';
         $bucketDatabaseName = $this->bucketResponse->getCreateBucketObjectName();
         $db = $this->getConnection($this->projectCredentials);
@@ -552,15 +457,25 @@ class ImportTableFromFileTest extends BaseCase
                 ->setEnclosure(CsvOptions::DEFAULT_ENCLOSURE)
                 ->setEscapedBy(CsvOptions::DEFAULT_ESCAPED_BY)
                 ->setSourceType(TableImportFromFileCommand\CsvTypeOptions\SourceType::SLICED_FILE)
-                ->setCompression(TableImportFromFileCommand\CsvTypeOptions\Compression::GZIP)
+                ->setCompression($compression)
         );
         $cmd->setFormatTypeOptions($formatOptions);
-        $cmd->setFilePath(
-            (new FilePath())
-                ->setRoot((string) getenv('AWS_S3_BUCKET'))
-                ->setPath('sliced/accounts-gzip')
-                ->setFileName('S3.accounts-gzip.csvmanifest')
-        );
+        if ($compression === TableImportFromFileCommand\CsvTypeOptions\Compression::GZIP) {
+            $cmd->setFilePath(
+                (new FilePath())
+                    ->setRoot((string) getenv('AWS_S3_BUCKET'))
+                    ->setPath('sliced/accounts-gzip')
+                    ->setFileName('S3.accounts-gzip.csvmanifest')
+            );
+        } else {
+            // no compression
+            $cmd->setFilePath(
+                (new FilePath())
+                    ->setRoot((string) getenv('AWS_S3_BUCKET'))
+                    ->setPath('sliced/accounts')
+                    ->setFileName('S3.accounts.csvmanifest')
+            );
+        }
         $credentials = new Any();
         $credentials->pack(
             (new S3Credentials())
