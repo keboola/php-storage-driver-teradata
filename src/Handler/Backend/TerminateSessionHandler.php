8d44a6a54c803848f7f72bc3d45fe0fd5b293516
zajca <zajca@zajca.cz>
2022-09-15 11:47:49 +0200

    add TerminateSessionCommand

---
+++ src/Handler/Backend/TerminateSessionHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageDriver\Teradata\Handler\Backend;
+
+use Google\Protobuf\Internal\Message;
+use Keboola\StorageDriver\Command\Common\TerminateSessionCommand;
+use Keboola\StorageDriver\Contract\Driver\Command\DriverCommandHandlerInterface;
+use Keboola\StorageDriver\Credentials\GenericBackendCredentials;
+use Keboola\StorageDriver\Teradata\TeradataSessionManager;
+
+final class TerminateSessionHandler implements DriverCommandHandlerInterface
+{
+    private TeradataSessionManager $manager;
+
+    public function __construct(TeradataSessionManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * @inheritDoc
+     * @param GenericBackendCredentials $credentials
+     */
+    public function __invoke(
+        Message $credentials,
+        Message $command,
+        array $features
+    ): ?Message {
+        assert($credentials instanceof GenericBackendCredentials);
+        assert($command instanceof TerminateSessionCommand);
+
+        $db = $this->manager->createSession($credentials);
+
+        // abort existing sessions
+        $db->executeStatement(sprintf(
+            "SELECT SYSLIB.AbortSessions (-1, '*', %s, 'Y', 'Y');",
+            $command->getSessionId()
+        ));
+    }
+}
