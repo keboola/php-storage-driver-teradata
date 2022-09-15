9096393d66947d3d7c711890d8b65fdb56f72b68
zajca <zajca@zajca.cz>
2022-09-15 20:39:14 +0200

    update protoc, update test.php to throw exception

--- test.php
+++ test.php
@@ -16,4 +16,12 @@ $json = '{"credentials":{"@type":"type.googleapis.com/keboola.storageDriver.cred
 $m = new \Keboola\StorageDriver\Command\Common\DriverRequest();
 
 $m->mergeFromJsonString($json);
-var_export($m->serializeToJsonString());
+$json = $m->serializeToJsonString();
+var_export($json);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+$m = new \Keboola\StorageDriver\Command\Common\DriverRequest();
+
+$m->mergeFromJsonString($json);
