d70101b38a7a6277a3d63afe1a87769ffd002f05
zajca <zajca@zajca.cz>
2022-09-16 10:48:55 +0200

    update test

--- test.php
+++ test.php
@@ -11,7 +11,63 @@ require __DIR__ . '/vendor/autoload.php';
 \GPBMetadata\Proto\Table::initOnce();
 \GPBMetadata\Proto\Workspace::initOnce();
 
-$json = '{"credentials":{"@type":"type.googleapis.com/keboola.storageDriver.credentials.GenericBackendCredentials","host":"xxx","principal":"xxx","secret":"xxx","meta":{"@type":"type.googleapis.com/keboola.storageDriver.credentials.GenericBackendCredentials.TeradataCredentialsMeta","database":"xxx"}},"command":{"@type":"type.googleapis.com/keboola.storageDriver.command.table.TableImportFromFileCommand","formatTypeOptions":{"@type":"type.googleapis.com/keboola.storageDriver.command.table.TableImportFromFileCommand.CsvTypeOptions","columnsNames":["id","name","city","sex"],"delimiter":",","enclosure":"\"","compression":"GZIP"},"filePath":{"root":"xxx","path":"xxx","fileName":"xxx"},"fileCredentials":{"@type":"type.googleapis.com/keboola.storageDriver.command.table.ImportExportShared.S3Credentials","key":"xxx","secret":"xxx","region":"eu-central-1"},"destination":{"path":["xxx"],"tableName":"xxx"},"importOptions":{"timestampColumn":"_timestamp","numberOfIgnoredLines":1,"dedupType":"INSERT_DUPLICATES"},"meta":{"@type":"type.googleapis.com/keboola.storageDriver.command.table.TableImportFromFileCommand.TeradataTableImportMeta"}}}';
+$jsonNoFileCredentials = '{
+    "credentials": {
+        "@type": "type.googleapis.com/keboola.storageDriver.credentials.GenericBackendCredentials",
+        "host": "xxx",
+        "principal": "xxx",
+        "secret": "xxx",
+        "meta": {
+            "@type": "type.googleapis.com/keboola.storageDriver.credentials.GenericBackendCredentials.TeradataCredentialsMeta",
+            "database": "xxx"
+        }
+    },
+    "command": {
+        "@type": "type.googleapis.com/keboola.storageDriver.command.table.TableImportFromFileCommand",
+        "filePath": {}
+    }
+}';
+
+$m = new \Keboola\StorageDriver\Command\Common\DriverRequest();
+
+$m->mergeFromJsonString($jsonNoFileCredentials);
+$json = $m->serializeToJsonString();
+var_export($json);
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+$m = new \Keboola\StorageDriver\Command\Common\DriverRequest();
+
+$m->mergeFromJsonString($json);
+
+
+echo PHP_EOL;
+echo 'This is OK.';
+echo PHP_EOL;
+echo 'Now with fileCredentials type.';
+echo PHP_EOL;
+
+$json = '{
+    "credentials": {
+        "@type": "type.googleapis.com/keboola.storageDriver.credentials.GenericBackendCredentials",
+        "host": "xxx",
+        "principal": "xxx",
+        "secret": "xxx",
+        "meta": {
+            "@type": "type.googleapis.com/keboola.storageDriver.credentials.GenericBackendCredentials.TeradataCredentialsMeta",
+            "database": "xxx"
+        }
+    },
+    "command": {
+        "@type": "type.googleapis.com/keboola.storageDriver.command.table.TableImportFromFileCommand",
+        
+        "filePath": {},
+        "fileCredentials": {
+            "@type": "type.googleapis.com/keboola.storageDriver.command.table.ImportExportShared.S3Credentials"
+        }
+    }
+}';
 
 $m = new \Keboola\StorageDriver\Command\Common\DriverRequest();
 
