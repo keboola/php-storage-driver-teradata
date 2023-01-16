b03915582db6c196c1cfb25135af0dcc2db8efaf
Jiri Semmler <jirka.semmler@gmail.com>
2023-01-16 13:43:30 +0100

    TER-42 Test for importFromFile

--- src/Handler/Exception/ExceptionResolver.php
+++ src/Handler/Exception/ExceptionResolver.php
@@ -3,6 +3,7 @@
 namespace Keboola\StorageDriver\Teradata\Handler\Exception;
 
 use Doctrine\DBAL\Exception as DBALException;
+use Doctrine\DBAL\Exception\DriverException;
 use Keboola\Db\ImportExport\Backend\Teradata\ToStage\Exception\NoMoreRoomInTDException;
 use ReflectionClass;
 
@@ -16,9 +17,13 @@ class ExceptionResolver
             case NoMoreRoomInTDException::class:
                 return NoSpaceException::createForFullDB($e);
             case DBALException::class:
+            case DriverException::class:
             {
                 if (strpos($e->getMessage(), 'PERMANENT space is invalid') !== false) {
                     return NoSpaceException::createForFullParent($e);
+                }
+                if (strpos($e->getMessage(), 'No more room in database') !== false) {
+                    return NoSpaceException::createForFullDB($e);
                 }
             }
         }
