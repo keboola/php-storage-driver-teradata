6169e2bcdf057a783d6f9a92ddd006f73c4a3a78
zajca <zajca@zajca.cz>
2023-04-03 16:03:50 +0200

    use php8.1

--- src/DebugLogger.php
+++ src/DebugLogger.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace Keboola\StorageDriver\Teradata;
 
 use Psr\Log\AbstractLogger;
+use Stringable;
 
 class DebugLogger extends AbstractLogger
 {
     /**
      * @inheritDoc
      * @param string $level
+     * @param array<mixed> $context
      */
-    public function log($level, $message, array $context = [])
+    public function log($level, $message, array $context = []): void
     {
         error_log(
             sprintf(
@@ -26,7 +28,7 @@ class DebugLogger extends AbstractLogger
     /**
      * @param mixed[] $context
      */
-    protected function interpolate(string $message, array $context = []): string
+    protected function interpolate(string|Stringable $message, array $context = []): string
     {
         // build a replacement array with braces around the context keys
         $replace = [];
@@ -37,6 +39,6 @@ class DebugLogger extends AbstractLogger
         }
 
         // interpolate replacement values into the message and return
-        return strtr($message, $replace);
+        return strtr((string) $message, $replace);
     }
 }
--- tests/functional/BaseCase.php
+++ tests/functional/BaseCase.php
@@ -63,9 +63,8 @@ class BaseCase extends TestCase
 
     /**
      * @param array<mixed> $data
-     * @param int|string $dataName
      */
-    public function __construct(?string $name = null, array $data = [], $dataName = '')
+    public function __construct(?string $name = null, array $data = [], int|string $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
         $this->sessionManager = new TeradataSessionManager(static::isDebug());
